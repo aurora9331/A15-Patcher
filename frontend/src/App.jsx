9d7d8e353ae1a4759b02a699c899914e10faddb7
aurora9331 <sevincemre993@gmail.com>
2025-08-29 16:30:30 +0300

    Vite (React) + Flask backend modern web arayüzü altyapısı: temel dosyalar ve kurulum dokümantasyonu eklendi

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UploadForm from './components/UploadForm'
+
+function App() {
+  return (
+    <div style={{ maxWidth: 600, margin: "40px auto", fontFamily: "sans-serif" }}>
+      <h1>A15 Patcher Yükleyici</h1>
+      <UploadForm />
+    </div>
+  )
+}
+
+export default App
